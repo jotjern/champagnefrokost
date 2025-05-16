43ad02dfe77d920715092c14692e7c7303c65c5b
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-05-16 19:55:25 +0200

    Add spotify integration

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken: string | null;
+  }
+}
